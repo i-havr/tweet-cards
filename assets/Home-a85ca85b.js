f9ee87b8eb4155d1d74b75adbd5b73c9872d4123
i-havr <i.havrylov@gmail.com>
2023-05-18 23:09:55 +0300

    Updates

---
+++ assets/Home-a85ca85b.js
@@ -0,0 +1,38 @@
+import{i as e,L as i,j as t}from"./index-dfc7eccc.js";const n=e.main`
+  display: block;
+  width: 100%;
+  min-height: 100%;
+`,o=e.section`
+  display: block;
+  width: 100%;
+  height: 100%;
+`,r=e.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  min-width: 80%;
+  min-height: 100%;
+  margin-left: auto;
+  margin-right: auto;
+  padding-top: 40vh;
+`,s=e.h1`
+  font-size: 32px;
+  margin-bottom: 14px;
+`,a=e(i)`
+  background: linear-gradient(
+    114.99deg,
+    #281d44 -0.99%,
+    #6e35f4 54.28%,
+    #3e0eaf 78.99%
+  );
+  background-clip: text;
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+
+  &:hover,
+  &:focus {
+    border-bottom: 3px solid #ebd8ff;
+    cursor: pointer;
+  }
+`;function l(){return t.jsx(n,{children:t.jsx(o,{children:t.jsxs(r,{children:[t.jsxs(s,{children:["Hi there!"," ",t.jsx("span",{role:"img","aria-label":"wawing hand",children:"👋"})]}),t.jsxs("h2",{children:["How about some"," ",t.jsx(a,{to:"/tweets",children:"tweets"}),"?"]})]})})})}export{l as default};
