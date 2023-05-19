a18567e490d6db5dc1798f35a8a2724710392bb8
i-havr <i.havrylov@gmail.com>
2023-05-19 19:11:53 +0300

    Updates

--- assets/Home-42bb1daa.js
+++ assets/Home-42bb1daa.js
@@ -1,4 +1,4 @@
-import{i as e,L as i,j as t}from"./index-db8a0b7a.js";const n=e.main`
+import{i as e,L as i,j as t}from"./index-9cd5bce3.js";const n=e.main`
   display: block;
   width: 100%;
   min-height: 100%;
